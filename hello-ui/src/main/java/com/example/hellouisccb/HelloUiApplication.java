c62b6754f4beaf88f0e1277e86cd9bc3a2d87bca
Masatoshi Tada <rosso.e.nero84@gmail.com>
2020-03-15 17:43:10 +0900

    不要なコードを削除

--- hello-ui/src/main/java/com/example/hellouisccb/HelloUiApplication.java
+++ hello-ui/src/main/java/com/example/hellouisccb/HelloUiApplication.java
@@ -1,38 +1,12 @@
 package com.example.hellouisccb;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.core.IntervalFunction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.Duration;
 
 @SpringBootApplication
 public class HelloUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloUiApplication.class, args);
-		// Constant Backoff
-		CircuitBreakerConfig config1 = CircuitBreakerConfig.custom()
-				.permittedNumberOfCallsInHalfOpenState(5)
-				.waitDurationInOpenState(Duration.ofSeconds(3))
-				.build();
-		// Randomized Backoff
-		CircuitBreakerConfig config2 = CircuitBreakerConfig.custom()
-				.permittedNumberOfCallsInHalfOpenState(5)
-				.waitIntervalFunctionInOpenState(IntervalFunction.ofRandomized())
-				.build();
-		// Exponential Backoff
-		CircuitBreakerConfig config3 = CircuitBreakerConfig.custom()
-				.permittedNumberOfCallsInHalfOpenState(5)
-				.waitIntervalFunctionInOpenState(
-						IntervalFunction.ofExponentialBackoff(Duration.ofSeconds(1), 2))
-				.build();
-		// Exponential Random Backoff
-		CircuitBreakerConfig config4 = CircuitBreakerConfig.custom()
-				.permittedNumberOfCallsInHalfOpenState(5)
-				.waitIntervalFunctionInOpenState(
-						IntervalFunction.ofExponentialRandomBackoff(Duration.ofSeconds(1), 2))
-				.build();
 	}
 }
